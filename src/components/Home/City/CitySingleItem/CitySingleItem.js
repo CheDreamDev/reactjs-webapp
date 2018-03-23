d908d0e39825b066cf7fb35d857b37b4ad66ea97
alenamandrika <Alenenok0@ukr.net>
2018-03-23 23:15:07 +0200

    code cleaning City

--- src/components/Home/City/CitySingleItem/CitySingleItem.js
+++ src/components/Home/City/CitySingleItem/CitySingleItem.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import { Col } from 'react-grid-system'
 
 const CitySingleItem = props => (
-  <Col md={3} className='rowCity block row-wrap'>
+  <div className='rowCity block'>
     <img className='photoCity' src={props.photo} alt='sky' />
     <div className='blockRowCity'>
       <h3 className='titleRowCity'>{props.title}</h3>
       <div className='counter'>{props.counter} мрії</div>
     </div>
-  </Col>
+  </div>
 )
 
 export default CitySingleItem
